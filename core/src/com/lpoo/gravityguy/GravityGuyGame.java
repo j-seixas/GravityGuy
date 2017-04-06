2563384bc4f35d6ede0d494a6aea292856bfdf95
rendoir <up201503822@fe.up.pt>
2017-04-06 16:35:25 +0100

    Started working on the player / world class

--- core/src/com/lpoo/gravityguy/GravityGuyGame.java
+++ core/src/com/lpoo/gravityguy/GravityGuyGame.java
@@ -3,8 +3,17 @@ package com.lpoo.gravityguy;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+
+import java.util.ArrayList;
 
 public class GravityGuyGame extends Game {
+
+    public static final int MAX_PLAYERS = 4;
+
+    private World world;
+    private ArrayList<Player> players;
 
     private MainMenuScreen mainMenu;
     private GameScreen gameScreen;
@@ -13,6 +22,10 @@ public class GravityGuyGame extends Game {
 	public void create () {
         mainMenu = new MainMenuScreen();
         gameScreen = new GameScreen();
+        world = new World(new Vector2(0f, -1f), true);
+        players = new ArrayList<Player>();
+        //TODO We should only init players after we know how many will play
+        players.add(new Player(world));
 	}
 
 	@Override
@@ -27,4 +40,6 @@ public class GravityGuyGame extends Game {
         mainMenu.dispose();
         gameScreen.dispose();
 	}
+
+
 }
